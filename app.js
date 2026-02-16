26a5cf8c79330b75edd902ca17d0c2f68a318e3d
Pavel Bolvinov <pbolvinov@gmail.com>
2026-02-16 22:01:23 +0300

    app.js

--- app.js
+++ app.js
@@ -12,15 +12,9 @@ if (tg.colorScheme === 'dark') {
 // Устанавливаем цвет заголовка
 tg.setHeaderColor('#4A90A4');
 
-// Функция для открытия бесплатного эфира
-function openWebinar() {
-    if (tg.HapticFeedback) {
-        tg.HapticFeedback.impactOccurred('medium');
-    }
-    tg.openLink('https://meleshkod.ru/webinar-2');
-}
+// ========== ПЛАТНЫЕ ПРОГРАММЫ ==========
 
-// Функция для открытия курса "Циклы Силы"
+// Циклы Силы
 function openCourse() {
     if (tg.HapticFeedback) {
         tg.HapticFeedback.impactOccurred('medium');
@@ -28,7 +22,59 @@ function openCourse() {
     tg.openLink('https://meleshkod.ru/ciklsily');
 }
 
-// Функция для открытия подкастов в Telegram
+// Программа для ВДА (взрослые дети алкоголиков)
+function openVDA() {
+    if (tg.HapticFeedback) {
+        tg.HapticFeedback.impactOccurred('medium');
+    }
+    tg.openLink('https://meleshkod.ru/growing-ground');
+}
+
+// Мастер личных границ
+function openBoundaries() {
+    if (tg.HapticFeedback) {
+        tg.HapticFeedback.impactOccurred('medium');
+    }
+    tg.openLink('https://meleshkod.ru/kurs/mastergranic');
+}
+
+// Сепарация. Путь к личной свободе
+function openSeparation() {
+    if (tg.HapticFeedback) {
+        tg.HapticFeedback.impactOccurred('medium');
+    }
+    tg.openLink('https://meleshkod.ru/separacia');
+}
+
+// Самооценка. Я окей - Ты окей
+function openSelfEsteem() {
+    if (tg.HapticFeedback) {
+        tg.HapticFeedback.impactOccurred('medium');
+    }
+    tg.openLink('https://meleshkod.ru/samoocenka');
+}
+
+// ========== БЕСПЛАТНЫЕ ЭФИРЫ ==========
+
+// Выход из модели созависимых отношений
+function openWebinar() {
+    if (tg.HapticFeedback) {
+        tg.HapticFeedback.impactOccurred('medium');
+    }
+    tg.openLink('https://meleshkod.ru/webinar-2');
+}
+
+// Эффективные инструменты самотерапии
+function openSelfTherapy() {
+    if (tg.HapticFeedback) {
+        tg.HapticFeedback.impactOccurred('medium');
+    }
+    tg.openLink('https://meleshkod.ru/web2');
+}
+
+// ========== ПОДКАСТЫ ==========
+
+// Подкасты в Telegram
 function openPodcastTelegram() {
     if (tg.HapticFeedback) {
         tg.HapticFeedback.impactOccurred('light');
@@ -36,7 +82,7 @@ function openPodcastTelegram() {
     tg.openLink('https://t.me/mavestreambot/app?startapp=meleshko');
 }
 
-// Функция для открытия подкастов на Яндекс.Музыке
+// Подкасты на Яндекс.Музыке
 function openPodcastYandex() {
     if (tg.HapticFeedback) {
         tg.HapticFeedback.impactOccurred('light');
@@ -44,7 +90,7 @@ function openPodcastYandex() {
     tg.openLink('https://music.yandex.ru/album/22198698');
 }
 
-// Функция для открытия подкастов в Apple Podcasts
+// Подкасты в Apple Podcasts
 function openPodcastApple() {
     if (tg.HapticFeedback) {
         tg.HapticFeedback.impactOccurred('light');
@@ -52,13 +98,68 @@ function openPodcastApple() {
     tg.openLink('https://podcasts.apple.com/ru/podcast/психология-подкасты-психотерапия-дмитрий-мелешко/id1616817624');
 }
 
-// Функция для открытия службы заботы
+// ========== СЛУЖБА ЗАБОТЫ ==========
+
+// Написать в Telegram
 function openSupport() {
     if (tg.HapticFeedback) {
         tg.HapticFeedback.impactOccurred('light');
     }
     tg.openTelegramLink('https://t.me/MeleshkoDmitrii');
 }
+
+// ========== КОНСУЛЬТАЦИЯ ПСИХОЛОГА ==========
+
+function openConsultation() {
+    if (tg.HapticFeedback) {
+        tg.HapticFeedback.impactOccurred('medium');
+    }
+    tg.openLink('https://meleshkod.ru/consultations');
+}
+
+// ========== СОЦИАЛЬНЫЕ СЕТИ ==========
+
+// YouTube канал
+function openYouTube() {
+    if (tg.HapticFeedback) {
+        tg.HapticFeedback.impactOccurred('light');
+    }
+    tg.openLink('https://www.youtube.com/channel/UCODeAI8gjTn28c72n5-MfvQ');
+}
+
+// Группа ВКонтакте
+function openVK() {
+    if (tg.HapticFeedback) {
+        tg.HapticFeedback.impactOccurred('light');
+    }
+    tg.openLink('https://vk.com/meleshkod');
+}
+
+// VK Видео
+function openVKVideo() {
+    if (tg.HapticFeedback) {
+        tg.HapticFeedback.impactOccurred('light');
+    }
+    tg.openLink('https://vkvideo.ru/@meleshkod');
+}
+
+// Дзен
+function openDzen() {
+    if (tg.HapticFeedback) {
+        tg.HapticFeedback.impactOccurred('light');
+    }
+    tg.openLink('https://dzen.ru/meleshkod');
+}
+
+// Rutube
+function openRutube() {
+    if (tg.HapticFeedback) {
+        tg.HapticFeedback.impactOccurred('light');
+    }
+    tg.openLink('https://rutube.ru/channel/28536025/');
+}
+
+// ========== СИСТЕМНЫЕ ФУНКЦИИ ==========
 
 // Уведомляем Telegram, что приложение готово
 tg.ready();
